bd93f701412590a52b24517601cbc6f9ed2eca51
mamikonyanN <Mamikonyan.N@yandex.ru>
2019-07-19 15:16:03 +0300

    First draft previews

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,18 +1,38 @@
 <template>
   <v-flex xs6 md4 lg3>
-    <v-img :aspect-ratio="16 / 9" :src="'https://cdn.vuetifyjs.com/images/parallax/material.jpg'">
-      <v-layout pa-2 column fill-height>
-        <v-flex>
-          <span class="pa-1" style="background: rgba(0,0,0,.6); border-radius:4px">
-            <v-icon small>play_arrow</v-icon> длительность
-          </span>
-        </v-flex>
-        <v-spacer></v-spacer>
-        <v-flex shrink>
-          <span class="pa-1 right" style="background: rgba(0,0,0,.6); border-radius:4px">время</span>
-        </v-flex>
+    <router-link to="/45" tag="div">
+      <v-img
+        :aspect-ratio="16 / 9"
+        :src="source"
+        @mouseover="onHover()"
+        @error="onMouseOut()"
+        @mouseout="onMouseOut()"
+        style="cursor: pointer"
+        lazy-src="https://picsum.photos/id/11/100/60"
+      >
+        <template v-slot:placeholder>
+          <v-layout fill-height align-center justify-center ma-0>
+            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+          </v-layout>
+        </template>
+        <v-layout pa-2 column fill-height>
+          <v-flex>
+            <span class="pa-1" style="background: rgba(0,0,0,.6); border-radius:4px">
+              <v-icon small>play_arrow</v-icon> {{ duration }}
+            </span>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex shrink>
+            <span class="pa-1 right" style="background: rgba(0,0,0,.6); border-radius:4px">{{ game }}</span>
+          </v-flex>
+        </v-layout>
+      </v-img>
+      <v-layout row nowrap>
+        <v-flex row style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis">{{ title }}</v-flex>
+        <v-spacer />
+        <v-flex class="text-xs-right">{{ date }}</v-flex>
       </v-layout>
-    </v-img>
+    </router-link>
   </v-flex>
 </template>
 
@@ -20,7 +40,7 @@
 export default {
   props: {
     id: String,
-    previewSequence: Array,
+    sequence: Array,
     game: String,
     title: String,
     date: Number,
@@ -28,8 +48,34 @@ export default {
   },
   data() {
     return {
-      test: "test"
+      defaultImage: `${process.env.VUE_APP_URL}/streams/olyashaa/${this.id}/preview.jpg`,
+      previews: this.sequence.map(item => {
+        return `${process.env.VUE_APP_URL}/streams/olyashaa/${this.id}/animated_preview/${item.src}`;
+      }),
+      source: `${process.env.VUE_APP_URL}/streams/olyashaa/${this.id}/preview.jpg`,
+      loop: setTimeout(() => {}, 0),
+      index: 1
     };
+  },
+  methods: {
+    onHover() {
+      this.index = 1;
+      this.source = this.previews[0];
+      this.circle();
+    },
+    onMouseOut() {
+      clearInterval(this.loop);
+      this.source = this.defaultImage;
+      this.index = 1;
+    },
+    circle() {
+      let vm = this;
+      this.loop = setTimeout(() => {
+        vm.source = vm.previews[this.index];
+        this.index = (this.index + 1) % (vm.previews.length - 1);
+        vm.circle();
+      }, 750);
+    }
   }
 };
 </script>
